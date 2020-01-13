b73e9b3af5b1cd485a46057bc346a8eb03c87769
shubhankarb180 <shubhankarb180@gmail.com>
2020-01-13 14:06:36 +0530

    refractor : Search Functionality refractored to Component
    Search Component is created from the search Functionality so the it can be used later
    if required in the project. Breaking down into components is a quality of a good react
    developer so hence it needed to be done.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 class App extends React.Component {
 
   constructor() {
@@ -24,7 +25,10 @@ class App extends React.Component {
     );
     return (
       <div className="App">
-        <input type="search" placeholder='search monsters' onChange={e => this.setState({ searchField : e.target.value })} />
+        <SearchBox 
+          placeholder='search monsters' 
+          handleChange={e => this.setState({ searchField : e.target.value })} 
+        />
         <CardList monsters={filteredMonsters} />
       </div>
     );

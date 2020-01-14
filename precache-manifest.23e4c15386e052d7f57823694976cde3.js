684a6cb1dabce65ec7e95dff7622d651b08782e1
shubhankarb180 <shubhankarb180@gmail.com>
2020-01-14 10:40:05 +0530

    Updates

---
+++ precache-manifest.23e4c15386e052d7f57823694976cde3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a253a015394fee4689a4bd9255099614",
+    "url": "/monsterdex/index.html"
+  },
+  {
+    "revision": "115f93f5bc5b3a405cb9",
+    "url": "/monsterdex/static/css/main.cc6bc4b2.chunk.css"
+  },
+  {
+    "revision": "b4f9b58947311975069b",
+    "url": "/monsterdex/static/js/2.4f49b48f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsterdex/static/js/2.4f49b48f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "115f93f5bc5b3a405cb9",
+    "url": "/monsterdex/static/js/main.c5de8645.chunk.js"
+  },
+  {
+    "revision": "792d9b6599d76da3d018",
+    "url": "/monsterdex/static/js/runtime-main.affaafd1.js"
+  }
+]);
